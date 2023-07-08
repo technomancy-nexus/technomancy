4525667837e188f0c7052c0cf56377b7e84f8500
Marcel Müller <neikos@neikos.email>
2023-07-08 15:54:42 +0200

    Add rudimentary login
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-use std::net::SocketAddr;
+use std::{collections::HashMap, net::SocketAddr, sync::Arc};
 
-use axum::{
-    routing::{get, post},
-    Json, Router,
+use axum::{routing::get, Extension, Router};
+use axum_login::{
+    extractors::AuthContext, memory_store::MemoryStore as AuthMemoryStore, AuthLayer,
+    RequireAuthorizationLayer,
 };
-use serde::Serialize;
+use axum_sessions::{async_session::MemoryStore as SessionMemoryStore, SessionLayer};
+
+use tokio::sync::RwLock;
+use user::User;
+
+mod user;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
-
-    let app = Router::new()
-        .route("/", get(root))
-        .route("/about", post(about));
+    let app = app();
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!(?addr, "listening");
@@ -23,17 +26,43 @@ async fn main() {
         .unwrap();
 }
 
-async fn root() -> &'static str {
-    "Hello, World!"
+type Auth = AuthContext<String, User, AuthMemoryStore<String, User>>;
+type RequireAuth = RequireAuthorizationLayer<String, User>;
+
+fn app() -> Router {
+    let secret = [0u8; 64];
+
+    let session_store = SessionMemoryStore::new();
+    let session_layer = SessionLayer::new(session_store, &secret);
+
+    let store = Arc::new(RwLock::new(HashMap::from([(
+        String::from("test"),
+        User {
+            name: "test".to_string(),
+        },
+    )])));
+
+    let user_store: AuthMemoryStore<String, User> = AuthMemoryStore::new(&store);
+    let auth_layer = AuthLayer::new(user_store, &secret);
+
+    Router::new()
+        .route("/", get(root))
+        .route_layer(RequireAuth::login())
+        .route("/login", get(login_handler))
+        .layer(auth_layer)
+        .layer(session_layer)
 }
 
-#[derive(Serialize)]
-struct About {
-    version: String,
+async fn root(Extension(user): Extension<User>) -> String {
+    format!("Hello {}", user.name)
 }
 
-async fn about() -> Json<About> {
-    Json(About {
-        version: String::from("0.0.0"),
+async fn login_handler(mut auth: Auth) -> &'static str {
+    auth.login(&User {
+        name: String::from("test"),
     })
+    .await
+    .unwrap();
+
+    "Logged in!"
 }
---
+++ src/user.rs
@@ -0,0 +1,16 @@
+use axum_login::{secrecy::SecretVec, AuthUser};
+
+#[derive(Debug, Clone)]
+pub struct User {
+    pub(crate) name: String,
+}
+
+impl AuthUser<String> for User {
+    fn get_id(&self) -> String {
+        self.name.clone()
+    }
+
+    fn get_password_hash(&self) -> axum_login::secrecy::SecretVec<u8> {
+        SecretVec::new(self.name.clone().into_bytes())
+    }
+}
